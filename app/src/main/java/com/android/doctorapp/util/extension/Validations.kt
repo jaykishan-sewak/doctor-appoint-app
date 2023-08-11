ff0b0bacff12906640870959f24357615f63172f
Namrata <namratacn@infusionanalysts.com>
2023-08-11 10:42:58 +0530

    Minor changes.

--- app/src/main/java/com/android/doctorapp/util/extension/Validations.kt
+++ app/src/main/java/com/android/doctorapp/util/extension/Validations.kt
@@ -1,8 +1,9 @@
 package com.android.doctorapp.util.extension
 
 fun String.isEmailAddressValid(): Boolean {
-    val pattern = Regex("^[A-Za-z0-9._-]+@[a-z]+.+[a-z]+")
+    //val pattern = Regex("^[A-Za-z0-9._-]+@[a-z]+.+[a-z]+")
     //val pattern = Regex("^[A-Za-z0-9+_.-]+@(.+)\$")
+    val pattern = Regex("^([a-zA-Z0-9]+)@([a-zA-Z]+)\\.([a-zA-Z]{2,5})$")
     //return this.trim().isNotEmpty() && PatternsCompat.EMAIL_ADDRESS.matcher(this).matches()
     return this.trim().isNotEmpty() && this.matches(pattern)
 }
@@ -10,7 +11,7 @@ fun String.isEmailAddressValid(): Boolean {
 fun String.isPassWordValid(): Boolean {
     val smallLetter = Regex("[a-z]")
     val capitalLetter = Regex("[A-Z]")
-    val specialLetter = Regex("[$@+-/#()_&]")
+    val specialLetter = Regex("[!#$@+%^*\\-=\\[\\]{};':?.<>/()_&]")
 
     return this.trim().isNotEmpty() && this.length >= 8 && smallLetter.containsMatchIn(this) &&
             capitalLetter.containsMatchIn(this) && specialLetter.containsMatchIn(this)
