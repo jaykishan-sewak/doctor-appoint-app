502c37d0e0715d6d6e9503f5020df1c070582d16
Namrata <namratacn@infusionanalysts.com>
2023-08-28 15:47:06 +0530

    conflict resolved.

--- app/src/main/java/com/android/doctorapp/ui/doctor/UpdateDoctorProfileFragment.kt
+++ app/src/main/java/com/android/doctorapp/ui/doctor/UpdateDoctorProfileFragment.kt
@@ -1,14 +1,18 @@
 package com.android.doctorapp.ui.doctor
 
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -19,21 +23,24 @@ import com.android.doctorapp.databinding.FragmentUpdateDoctorProfileBinding
 import com.android.doctorapp.di.AppComponentProvider
 import com.android.doctorapp.di.base.BaseFragment
 import com.android.doctorapp.di.base.toolbar.FragmentToolbar
+import com.android.doctorapp.util.constants.ConstantKey.BundleKeys.IS_DOCTOR_OR_USER_KEY
 import com.android.doctorapp.util.constants.ConstantKey.BundleKeys.STORED_VERIFICATION_Id_KEY
 import com.android.doctorapp.util.constants.ConstantKey.BundleKeys.USER_CONTACT_NUMBER_KEY
 import com.android.doctorapp.util.extension.alert
 import com.android.doctorapp.util.extension.neutralButton
 import com.android.doctorapp.util.extension.selectDate
 import com.android.doctorapp.util.extension.toast
+import com.google.android.material.chip.Chip
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.gson.Gson
 import kotlinx.coroutines.launch
+import java.util.Calendar
 import java.util.Date
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
-
 
 class UpdateDoctorProfileFragment :
     BaseFragment<FragmentUpdateDoctorProfileBinding>(R.layout.fragment_update_doctor_profile) {
@@ -41,22 +48,26 @@ class UpdateDoctorProfileFragment :
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel by viewModels<AddDoctorViewModel> { viewModelFactory }
-
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private val TAG = UpdateDoctorProfileFragment::class.java.simpleName
+    lateinit var storedVerificationId: String
+    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    lateinit var mTimePicker: TimePickerDialog
+    val mcurrentTime = Calendar.getInstance()
+    val hour = mcurrentTime.get(Calendar.HOUR_OF_DAY)
+    val minute = mcurrentTime.get(Calendar.MINUTE)
     val handler = Handler(Looper.getMainLooper())
     private val runnable = object : Runnable {
         override fun run() {
             viewModel.viewModelScope.launch {
                 viewModel.checkIsEmailEveryMin()
             }
-            handler.postDelayed(this, 30000)
+            handler.postDelayed(this, 10000)
         }
     }
-
-
-    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
-    private val TAG = UpdateDoctorProfileFragment::class.java.simpleName
-    lateinit var storedVerificationId: String
-    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    lateinit var bindingView: FragmentUpdateDoctorProfileBinding
+    var enteredDegreeText: String = ""
+    var enteredSpecializationText: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,6 +91,10 @@ class UpdateDoctorProfileFragment :
         super.onCreateView(inflater, container, savedInstanceState)
         handler.postDelayed(runnable, 1000)
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onCodeAutoRetrievalTimeOut(str: String) {
+                viewModel.hideProgress()
+            }
+
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
                 viewModel.hideProgress()
             }
@@ -103,7 +118,6 @@ class UpdateDoctorProfileFragment :
                     R.id.action_updateDoctorFragment_to_OtpVerificationFragment,
                     bundle
                 )
-
             }
         }
         mTimePicker = TimePickerDialog(
@@ -124,18 +138,15 @@ class UpdateDoctorProfileFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpWithViewModel(viewModel)
-        handler.postDelayed(runnable, 1000)
         registerObserver()
     }
 
     private fun registerObserver() {
-
         viewModel.getModelUserData().observe(viewLifecycleOwner) {
             viewModel.name.value = it[0].name
             viewModel.email.value = it[0].email
             viewModel.contactNumber.value = it[0].contactNumber
         }
-
         viewModel.clickResponse.observe(viewLifecycleOwner) {
             sendVerificationCode("+91$it")
         }
@@ -197,13 +208,11 @@ class UpdateDoctorProfileFragment :
                 context?.toast(requireContext().resources.getString(R.string.verification_main_sent))
             }
         }
-
         viewModel.isUserReload.observe(viewLifecycleOwner) {
             if (it == true) {
                 viewModel.emailVerified()
             }
         }
-
         viewModel.isEmailVerified.observe(viewLifecycleOwner) {
             if (it == true) {
                 viewModel.validateAllUpdateField()
