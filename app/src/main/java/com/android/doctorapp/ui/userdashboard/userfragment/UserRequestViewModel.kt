38907777dcd3777360ce677ffc10f4643b5e05d9
Namrata <namratacn@infusionanalysts.com>
2023-09-08 17:05:34 +0530

    Fixed issues added firebase crashlytics dependencies.

---
+++ app/src/main/java/com/android/doctorapp/ui/userdashboard/userfragment/UserRequestViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.doctorapp.ui.userdashboard.userfragment
+
+import com.android.doctorapp.di.base.BaseViewModel
+import javax.inject.Inject
+
+class UserRequestViewModel @Inject constructor() : BaseViewModel() {
+}
