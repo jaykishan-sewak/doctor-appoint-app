5e7f19cd1c58cf0423621c9512631d95f1aa5e93
Vikas Pandey <vikasrp@infusionanalysts.com>
2023-09-20 11:17:13 +0530

    PR review fail issue resolved

--- app/src/main/java/com/android/doctorapp/ui/doctor/UpdateDoctorProfileFragment.kt
+++ app/src/main/java/com/android/doctorapp/ui/doctor/UpdateDoctorProfileFragment.kt
@@ -205,12 +205,12 @@ class UpdateDoctorProfileFragment :
         }
 
         viewModel.userResponse.observe(viewLifecycleOwner) {
-            viewModel.name.value = it.name
-            viewModel.email.value = it.email
-            viewModel.contactNumber.value = it.contactNumber
-            viewModel.address.value = it.address
-            viewModel.selectGenderValue.value = it.gender
-            viewModel.dob.value = dateFormatter(it.dob, ConstantKey.DATE_MM_FORMAT)
+            viewModel.name.value = it?.name
+            viewModel.email.value = it?.email
+            viewModel.contactNumber.value = it?.contactNumber
+            viewModel.address.value = it?.address
+            viewModel.selectGenderValue.value = it?.gender
+            viewModel.dob.value = dateFormatter(it?.dob, ConstantKey.DATE_MM_FORMAT)
         }
         viewModel.clickResponse.observe(viewLifecycleOwner) {
             sendVerificationCode("+91$it")
--- app/src/main/java/com/android/doctorapp/ui/profile/AddUserProfileFragment.kt
+++ app/src/main/java/com/android/doctorapp/ui/profile/AddUserProfileFragment.kt
@@ -211,12 +211,12 @@ class AddUserProfileFragment :
         }
 
         viewModel.userResponse.observe(viewLifecycleOwner) {
-            viewModel.name.value = it.name
-            viewModel.email.value = it.email
-            viewModel.contactNumber.value = it.contactNumber
-            viewModel.address.value = it.address
-            viewModel.selectGenderValue.value = it.gender
-            viewModel.dob.value = dateFormatter(it.dob, ConstantKey.DATE_MM_FORMAT)
+            viewModel.name.value = it?.name
+            viewModel.email.value = it?.email
+            viewModel.contactNumber.value = it?.contactNumber
+            viewModel.address.value = it?.address
+            viewModel.selectGenderValue.value = it?.gender
+            viewModel.dob.value = dateFormatter(it?.dob, ConstantKey.DATE_MM_FORMAT)
         }
 
         viewModel.clickResponse.observe(viewLifecycleOwner) {
