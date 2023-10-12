704fb607b9ccf1aec22442050e3498c5fa9a8a90
Namrata <namratacn@infusionanalysts.com>
2023-10-12 17:53:22 +0530

    Completed task of [User] Book appointment UI changes according to new Ui design.

--- app/src/main/java/com/android/doctorapp/util/constants/ConstantKey.kt
+++ app/src/main/java/com/android/doctorapp/util/constants/ConstantKey.kt
@@ -25,7 +25,7 @@ object ConstantKey {
         const val DOCTOR_PROFILE_FRAGMENT = "doctorProfileFragment"
         const val FROM_WHERE = "fromWhere"
         const val SELECTED_TAB = "selectedTab"
-
+        const val BOOK_APPOINTMENT_DATA = "bookAppointmentData"
 
     }
 
